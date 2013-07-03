313272895301305934438efd042ffa7edbbf6c24
Zi Yang <ziy@cs.cmu.edu>
2013-07-03 17:38:30 -0400

    Added AbstractParseGenerator and AbstractParseEvidencer #12

---
+++ src/main/java/edu/cmu/lti/oaqa/baseqa/gerpphase/question/AbstractParseEvidencer.java
@@ -0,0 +1,8 @@
+package edu.cmu.lti.oaqa.baseqa.gerpphase.question;
+
+import edu.cmu.lti.oaqa.baseqa.data.nlp.ParseWrapper;
+import edu.cmu.lti.oaqa.baseqa.gerpphase.core.evidencer.AbstractIndependentEvidencer;
+
+public abstract class AbstractParseEvidencer extends AbstractIndependentEvidencer<ParseWrapper> {
+
+}
---
+++ src/main/java/edu/cmu/lti/oaqa/baseqa/gerpphase/question/AbstractParseGenerator.java
@@ -0,0 +1,32 @@
+package edu.cmu.lti.oaqa.baseqa.gerpphase.question;
+
+import java.util.List;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.cas.TOP;
+
+import com.google.common.collect.Lists;
+
+import edu.cmu.lti.oaqa.baseqa.data.core.TopWrapper;
+import edu.cmu.lti.oaqa.baseqa.data.nlp.ParseWrapper;
+import edu.cmu.lti.oaqa.baseqa.data.nlp.QuestionWrapper;
+import edu.cmu.lti.oaqa.baseqa.gerpphase.core.generator.AbstractGenerator;
+
+public abstract class AbstractParseGenerator extends AbstractGenerator<ParseWrapper> {
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public List<Class<? extends TopWrapper<?>>> getRequiredInputTypes() {
+    return Lists.<Class<? extends TopWrapper<?>>> newArrayList(QuestionWrapper.class);
+  }
+
+  @Override
+  public ParseWrapper generate(List<? extends TopWrapper<? extends TOP>> input)
+          throws AnalysisEngineProcessException {
+    QuestionWrapper question = (QuestionWrapper) input.get(0);
+    return generate(question);
+  }
+
+  protected abstract ParseWrapper generate(QuestionWrapper question);
+
+}
