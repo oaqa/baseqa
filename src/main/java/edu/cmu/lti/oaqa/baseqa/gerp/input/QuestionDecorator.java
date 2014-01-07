c3205f83136ce5d9d2663b713498c43e50bd735c
ziy <sincerezi@gmail.com>
2014-01-07 14:26:16 -0500

    QuestionDecorator now can be turned off automatically if Question already exists in CAS.

--- src/main/java/edu/cmu/lti/oaqa/baseqa/gerp/input/QuestionDecorator.java
+++ src/main/java/edu/cmu/lti/oaqa/baseqa/gerp/input/QuestionDecorator.java
@@ -2,6 +2,7 @@ package edu.cmu.lti.oaqa.baseqa.gerp.input;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.oaqa.model.input.Question;
 
@@ -16,6 +17,9 @@ public class QuestionDecorator extends JCasAnnotator_ImplBase {
 
   @Override
   public void process(JCas jcas) throws AnalysisEngineProcessException {
+    if (JCasUtil.select(jcas, Question.class).size() > 0) {
+      return;
+    }
     InputElement input = ProcessingStepUtils.getInputElement(jcas);
     QuestionWrapper question = new QuestionWrapper(input.getBegin(), input.getEnd(),
             input.getSequenceId(), input.getDataset(), input.getQuestion(),
@@ -23,7 +27,7 @@ public class QuestionDecorator extends JCasAnnotator_ImplBase {
     if (question != null) {
       WrapperIndexer indexer = new WrapperIndexer();
       Question annotation = WrapperHelper.unwrap(indexer, question, jcas);
-      annotation.addToIndexes();
+      annotation.addToIndexes(jcas);
     }
   }
 
