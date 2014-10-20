a413553410a7a215e4987a3aa62e2f9bf26fc392
ziy <sincerezi@gmail.com>
2014-10-19 23:36:45 -0400

    Added a util method to create object from a class path defined in context.

--- src/main/java/edu/cmu/lti/oaqa/baseqa/util/UimaContextHelper.java
+++ src/main/java/edu/cmu/lti/oaqa/baseqa/util/UimaContextHelper.java
@@ -1,5 +1,8 @@
 package edu.cmu.lti.oaqa.baseqa.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 import org.apache.uima.UimaContext;
 import org.apache.uima.resource.ResourceInitializationException;
 
@@ -60,17 +63,6 @@ public class UimaContextHelper {
     }
   }
 
-  public static Object getConfigParameterClassInstance(UimaContext aContext, String paramName,
-          String defaultClassName) {
-    String className = (String) getConfigParameterStringValue(aContext, paramName, defaultClassName);
-    try {
-      return Class.forName(className).newInstance();
-    } catch (Exception e) {
-      System.out.println(" * Class \"" + className + "\" cannot be set");
-      return new Object();
-    }
-  }
-
   public static String getConfigParameterStringValue(UimaContext aContext, String paramName)
           throws ResourceInitializationException {
     String value = (String) aContext.getConfigParameterValue(paramName);
@@ -91,6 +83,18 @@ public class UimaContextHelper {
     return value;
   }
 
+  public static String[] getConfigParameterStringArrayValue(UimaContext aContext, String paramName,
+          String[] defaultValue) throws ResourceInitializationException {
+    String[] value = (String[]) aContext.getConfigParameterValue(paramName);
+    if (value != null) {
+      return value;
+    } else {
+      System.out.println(" * Parameter \"" + paramName + "\" cannot be found, use default value \""
+              + defaultValue + "\" instead.");
+      return defaultValue;
+    }
+  }
+
   public static String[] getConfigParameterStringArrayValue(UimaContext aContext, String paramName)
           throws ResourceInitializationException {
     String[] value = (String[]) aContext.getConfigParameterValue(paramName);
@@ -99,6 +103,34 @@ public class UimaContextHelper {
               "Must provide a parameter of type <" + paramName + ">"));
     }
     return value;
+  }
+
+  public static <T> Class<? extends T> getConfigParameterClassValue(UimaContext aContext,
+          String paramName, Class<? extends T> defaultValue, Class<T> superClass) {
+    String className = (String) getConfigParameterStringValue(aContext, paramName,
+            defaultValue.getName());
+    try {
+      return Class.forName(className).asSubclass(superClass);
+    } catch (Exception e) {
+      System.out.println(" * Class \"" + className + "\" cannot be set");
+      return defaultValue;
+    }
+  }
+
+  public static <T> T createObjectFromConfigParameter(UimaContext aContext, String classParamName,
+          String constructorParamsParamName, Class<? extends T> defaultClass, Class<T> superClass)
+          throws ResourceInitializationException {
+    Class<? extends T> clazz = UimaContextHelper.getConfigParameterClassValue(aContext,
+            classParamName, defaultClass, superClass);
+    String[] params = UimaContextHelper.getConfigParameterStringArrayValue(aContext,
+            constructorParamsParamName, new String[0]);
+    Class<?>[] paramTypes = Arrays.stream(params).map(p -> String.class).toArray(Class[]::new);
+    try {
+      return clazz.getConstructor(paramTypes).newInstance((Object[]) params);
+    } catch (NoSuchMethodException | SecurityException | InstantiationException
+            | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+      throw new ResourceInitializationException(e);
+    }
   }
 
 }
